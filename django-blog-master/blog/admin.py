8b92612d34901a722ec4be5853051fa14de18280
LiuYvNing <1504738381@qq.com>
2022-05-03 14:43:56 +0800

    Django-blog

--- django-blog-master/blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from blog import models
-
-admin.site.register(models.User)
-admin.site.register(models.Article)
-admin.site.register(models.Comments)
